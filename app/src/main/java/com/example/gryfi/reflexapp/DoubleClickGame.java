e8ca0c70dece52268be0a86d099e5a69f8ce1e45
kamilgrygiel <kamilgrygiel96@gmail.com>
2019-02-02 18:47:52 +0100

    Four games withouth Validation

---
+++ app/src/main/java/com/example/gryfi/reflexapp/DoubleClickGame.java
@@ -0,0 +1,13 @@
+package com.example.gryfi.reflexapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoubleClickGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_double_click_game);
+    }
+}
---
+++ app/src/main/java/com/example/gryfi/reflexapp/ReflexSecondGame.java
@@ -0,0 +1,13 @@
+package com.example.gryfi.reflexapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReflexSecondGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reflex_second_game);
+    }
+}
